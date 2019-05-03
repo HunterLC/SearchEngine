ee679cfbe2cad5f29c05cd9ca9d3bebcd7f9a005
Hunter_LC <lc411887055@gmail.com>
2019-05-03 16:39:07 +0800

    first commited

---
+++ src/crawler/GetURL.java
@@ -0,0 +1,90 @@
+package crawler;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class GetURL {
+	/**
+	 * a标签中有四种值，需要判断
+	 * 1.只有路径：/citylist.html
+	 * 2.含有js代码：javascript:void(0)
+	 * 3.网址全称：http://www.xuecheyi.com/Info/List-83.html
+	 * 4.没有后缀/Info
+	 */
+	public static void getUrl(String uri,String filePath,String fileName) throws Exception {
+		List<String> list = new ArrayList<>();// 用list来存放地址
+		URL url = new URL(uri);
+		String protocol = url.getProtocol();// 获取协议
+		String host = url.getHost();// 获取域名
+		Document doc = Jsoup.connect(uri).get();// dom解析html
+		Elements ele = doc.getElementsByTag("a");// 获取网页中的a标签
+		for (Element a : ele) {// 遍历
+			String href = a.attr("href");
+			String reg = "[a-zA-z]+://[^\\s]*";
+			Pattern p = Pattern.compile(reg);
+			Matcher m = p.matcher(href);
+			if (m.find()) {// 通过正则表达式匹配了第三种http://jx.xuecheyi.com/member/login/index
+				DuplicationCheck(list,href);
+			} else if (href.indexOf("/") == 0) {// 匹配第一、四两种
+				String NewURL = protocol + "://" + host + href;
+				DuplicationCheck(list,NewURL);// /login/ind 0123456789 匹配出来的地址需要在前面加上协议和域名
+			}
+		}
+		
+		SaveFile(list,filePath,fileName);  //文件保存
+		//return list;
+	}
+
+	
+	/**
+	 * URL去重
+	 */
+	public static void DuplicationCheck(List<String> list, String url) {
+		boolean findURL = false;//查找标志位
+		if(list.size()==0) //列表为空，则直接添加
+			list.add(url);
+		else {
+			for(String item:list)
+				if(item.equals(url)) {  //已经存在该URL
+					findURL = true;
+					break;
+				}
+			if(!findURL)
+				list.add(url);
+		}
+	}
+	
+	/**
+	 * 将URL利用文件保存起来
+	 * @param list
+	 */
+	public static void SaveFile(List<String> list,String filePath,String fileName) {
+		File file=new File(filePath);		
+		if(!file.exists()){//如果文件夹不存在			
+			file.mkdirs();//创建文件夹	
+			//System.out.println("Done");
+		}		
+		try{//异常处理					
+			BufferedWriter bw=new BufferedWriter(new FileWriter(filePath+"/"+fileName,true));
+			for(String item:list)
+				bw.write(item+"\n");			
+			bw.close();//关闭文件		
+			}catch(IOException e){			
+				e.printStackTrace();		
+		}
+
+	}
+}
---
+++ src/crawler/WebDownload.java
@@ -0,0 +1,85 @@
+package crawler;
+
+import java.net.*;
+import java.io.*;
+public class WebDownload {
+	private String webURL; //网页URL
+	private String webName; //网页文件命名
+	
+	public WebDownload(String webURL,String webName) {
+		this.webURL = webURL;
+		this.webName = webName;
+	}
+	
+	public void DownLoad() throws IOException {
+		try {
+			URL webPage = new URL(webURL);  //构造URL
+			URLConnection con = webPage.openConnection();
+			BufferedReader urlReader = new BufferedReader(new InputStreamReader(con.getInputStream(),"utf-8"));
+			String content = "";
+			String line = "";
+			while((line = urlReader.readLine())!=null) {
+				content += line;
+				content += "\n";
+			}
+			urlReader.close();
+			File out = new File(webName);  //输出临时文件
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out),"utf-8"));
+			bw.write(content);
+			bw.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		//下载URL并保存到指定位置
+		String urlE = "https://github.com"; //英文网站
+		String urlC = "https://www.qq.com/";//中文网站
+		String urlFilePath = "F:/Eclipse/eclipse/code/SearchEngine/WebPage/URL";
+		String urlFileNameE = "URL1.txt";
+		String urlFileNameC = "URL2.txt";
+		GetURL.getUrl(urlE,urlFilePath,urlFileNameE);
+		GetURL.getUrl(urlC,urlFilePath,urlFileNameC);
+		
+		//创建中英文网页文档目录
+		String webFilePathE = "F:/Eclipse/eclipse/code/SearchEngine/WebPage/EnglishPage";
+		String webFilePathC = "F:/Eclipse/eclipse/code/SearchEngine/WebPage/ChinesePage";
+		File file1=new File(webFilePathE);		
+		if(!file1.exists()){//如果文件夹不存在			
+			file1.mkdirs();//创建文件夹	
+			System.out.println("Done");
+		}
+		File file2=new File(webFilePathC);
+		if(!file2.exists()){//如果文件夹不存在			
+			file2.mkdirs();//创建文件夹	
+			System.out.println("Done");
+		}
+		for (int i = 2; i >= 2; i--) {
+			File urlFile = new File(urlFilePath + "/URL"+i+".txt");
+			BufferedReader urlReader = new BufferedReader(new InputStreamReader(new FileInputStream(urlFile), "utf-8"));
+			int count = 0;
+			while (true) {
+				try {
+					String line = urlReader.readLine();
+					if (line == null)
+						break;
+					else {
+						String webName = null;
+						if(i == 1)
+							webName = webFilePathE+"/Web_E_" + (++count)+ ".txt";
+						else if(i == 2)
+							webName = webFilePathC+"/Web_C_" + (++count)+ ".txt";
+						WebDownload web = new WebDownload(line, webName);
+						web.DownLoad();
+						System.out.println(count + "下载完毕");
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			urlReader.close();
+		}
+	}
+}
